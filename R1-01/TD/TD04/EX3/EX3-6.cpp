07bc6e8eedde5b357ced66ebc11113cb08fe8940
Rabah N <94785703+RabahSend@users.noreply.github.com>
2022-11-01 17:25:57 +0100

    Update EX3-6.cpp
    fixing error

--- R1-01/TD/TD04/EX3/EX3-6.cpp
+++ R1-01/TD/TD04/EX3/EX3-6.cpp
@@ -9,7 +9,7 @@ void removeZero(int val[], size_t& size) { //Retire les 0 d'un tableau
     size_t write = 0;
     for (size_t read=0; read<size; read++) { //Lis chaque case du tableau
         if(val[read] == 0)
-            read++; //Avance si la valeur est 0
+            continue; //Avance si la valeur est 0
         else 
             val[write++]=val[read]; //Écrit la dernière valeur en train d'être lue dans un case libre (0)
     }
