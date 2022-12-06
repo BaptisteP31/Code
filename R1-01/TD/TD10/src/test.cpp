a4b8b6559a582a3c0017f583ab8efe0dfe0ed691
Baptiste P <paqueriaudbaba@gmail.com>
2022-12-06 23:57:09 +0100

    Update test.cpp

--- R1-01/TD/TD10/src/test.cpp
+++ R1-01/TD/TD10/src/test.cpp
@@ -1,7 +1,6 @@
 #include "../includes/rpn.hpp"
 
 int main() {
-    //std::vector<std::string> expression = {"2", "3", "-"};
     std::string str = "2 + 5 * 8";
     rpn::Expression exp = rpn::stringToExpression(str);
     rpn::infixToPostfix(exp);
